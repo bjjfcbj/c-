cc8664c5ea25909fb77b03c6e43e28abbbe40238
bjjfcbj <bjjfcbj@gmail.com>
2019-10-23 22:14:38 +0800

    ..

--- pat/pat1033.cc
+++ pat/pat1033.cc
@@ -12,34 +12,7 @@
 
 using namespace std;
 
-struct node
-{
-	int dis;
-	float pri;
-
-	node() = default;
-	node(int a, float b): dis(a), pri(b) {}
-
-	bool operator<(node& n)
-	{
-		return this->dis < n.dis;
-	}
-};
-
 int main()
 {
-	int C, D, L, N, d;
-	float P;
-	vector<node> vec;
-	scanf("%d %d %d %d", &C, &D, &L, &N);
-	while (N--)
-	{
-		scanf("%f %d", &P, &d);
-		vec.push_back(node(d, P));
-	}
-	sort(vec.begin(), vec.end());
-	/*
-	 *
-	 */
 	return 0;
 }
